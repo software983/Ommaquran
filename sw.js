46190ee16765dec149559b07f035699ec0fea23b
software983 <software.samsaf@gmail.com>
2026-07-22 16:46:19 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'moshaf-al-omma-v2.7'; // تغيير الرقم عند كل تحديث رئيسي (v2.5: شاشة القراءة والمزامنة)
+const CACHE_NAME = 'moshaf-al-omma-v2.8'; // تغيير الرقم عند كل تحديث رئيسي (v2.5: شاشة القراءة والمزامنة)
 
 // الملفات الأساسية
 const ASSETS_TO_CACHE = [
